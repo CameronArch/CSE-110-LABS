556e75ef754f937d1ac71f59efe4e5a641b8ae4f
CameronArch <cameronarch598@gmail.com>
2024-10-07 20:59:57 -0700

    React setup

--- CSE-110-Lab-2/my-app/src/App.tsx
+++ CSE-110-Lab-2/my-app/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
+import { ClickCounter, HeartButton, ToggleTheme } from "./hooksExercise";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+ return (
+    <div className='app-container'>
+        
+        <form className="note-form">
+            <div><input placeholder="Note Title"></input></div>
+
+            <div><textarea></textarea></div>
+
+            <div><button type="submit">Create Note</button></div>
+        </form>
+        
+        <div className="notes-grid">
+       {dummyNotesList.map((note) => (
+         <div
+           key={note.id}
+           className="note-item">
+           <div className="notes-header">
+                <HeartButton />
+             <button>x</button>
+           </div>
+           <h2> {note.title} </h2>
+           <p> {note.content} </p>
+           <p> {note.label} </p>
+         </div>
+         
+       ))}
+    <div>
+        <h2>List of Favorites:</h2>
+        {dummyNotesList.map((note) => (
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            {note.favorite ? note.title : null}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        ))}
     </div>
-  );
+    </div>
+     <ClickCounter />
+    </div>
+
+ );
 }
 
 export default App;
